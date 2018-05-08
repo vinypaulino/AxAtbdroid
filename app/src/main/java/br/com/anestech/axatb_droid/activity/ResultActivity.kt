4a0b04d18118cad2956bda7489052a05971d1689
Vinicius Paulino <vinydos3@gmail.com>
2018-05-08 18:37:08 -0300

    Adicionados layout(fragment) do resultado do app

---
+++ app/src/main/java/br/com/anestech/axatb_droid/activity/ResultActivity.kt
@@ -0,0 +1,13 @@
+package br.com.anestech.axatb_droid.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.com.anestech.axatb_droid.R
+
+class ResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
